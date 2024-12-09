74b90a6c20a4e24bc04d9b7643e91bbecd035ea5
Ciara Trigg <ciaratrigg20@gmail.com>
2024-12-09 00:52:32 -0500

    deserialization defense using ValidatingObjectInputStream

--- teamproject/src/main/java/com/se518/teamproject/FileUploadController.java
+++ teamproject/src/main/java/com/se518/teamproject/FileUploadController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.io.serialization.ValidatingObjectInputStream;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 
 @Controller
 public class FileUploadController {
     private static final String UPLOAD_DIR = "/data/";
+
         @PostMapping("/upload")
         public String handleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
             try {
@@ -21,15 +24,26 @@ public class FileUploadController {
                 }
                 File tempFile = new File(uploadDir, file.getOriginalFilename());
                 file.transferTo(tempFile);
-    
+
+                //secure deserialization
+                try(FileInputStream fis = new FileInputStream(tempFile);
+                    ValidatingObjectInputStream vois = new ValidatingObjectInputStream(fis)) {
+
+                    vois.accept(SimpleData.class);
+                    Object obj = vois.readObject();
+                    model.addAttribute("message", obj);
+                    model.addAttribute("messageType", "success");
+                }
+
                 // Deserialize the file
-                try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(tempFile))) {
+                /*try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(tempFile))) {
                     Object obj = ois.readObject();
                     model.addAttribute("message", obj.toString());
-                }
+                }*/
             } catch (Exception e) {
                 e.printStackTrace();
-                model.addAttribute("message", "Error deserializing file: " + e.getMessage());
+                model.addAttribute("message", "Error deserializing file. Class name not accepted");
+                model.addAttribute("messageType", "error");
             }
             return "FileUpload"; 
         }
